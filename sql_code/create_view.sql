74be043fd8eaca721a604281b3aafa0abae210a8
Muchammad Malik <64340098+mmalik17@users.noreply.github.com>
2026-01-22 17:58:47 +0700

    Create SQL view for completed orders
    Added a SQL script to create a view for completed orders.

---
+++ sql_code/create_view.sql
@@ -0,0 +1,24 @@
+------- TEMPLATE CODE ---------
+CREATE VIEW <nama_view> AS
+<insert_script_anda_disini>
+  
+------- EXAMPLE CODE ----------
+CREATE VIEW test_view AS
+select
+	o.order_id,
+	u.name,
+	p.product_name,
+	o.order_amount,
+	o.payment_method,
+		o.status,
+	p.price
+from
+		orders o
+join products p on
+	p.product_id = o.product_id
+join users u on
+	u.user_id = o.user_id
+where
+	o.status = 'Completed'
+order by
+	order_id;
